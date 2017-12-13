56f8cb652c27b253195d79edc0c9109dc38295f1
Chirishman <Chirishman@users.noreply.github.com>
2017-12-13 14:05:30 -0500

    Add files via upload

---
+++ Public/Get-Folder.ps1
@@ -0,0 +1,10 @@
+﻿function Get-Folder($InitialDirectory='MyDocuments'){
+    Add-Type -AssemblyName System.Windows.Forms
+    $openFolderDialog = New-Object System.Windows.Forms.FolderBrowserDialog
+    $openFolderDialog.ShowNewFolderButton = $true
+    $openFolderDialog.RootFolder = $InitialDirectory
+    $result = $openFolderDialog.ShowDialog()
+    if ($result -eq [System.Windows.Forms.DialogResult]::OK){
+        $openFolderDialog.SelectedPath
+    }
+}
